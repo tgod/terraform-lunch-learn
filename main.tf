eda1ac487a50551b2646721a52b6c9f7363f8291
Angel Villalain <angel.villalain@gmail.com>
2017-11-05 16:23:53 +0100

    Add Auto Scaling Group

--- main.tf
+++ main.tf
@@ -52,19 +52,44 @@ data "aws_ami" "ec2_ami" {
     "099720109477"]
 }
 
-resource "aws_instance" "web" {
-  count = "${length(var.az)}"
-  ami = "${data.aws_ami.ec2_ami.id}"
+resource "aws_launch_configuration" "web_asg" {
+  name = "web-asgc"
+  image_id = "${data.aws_ami.ec2_ami.id}"
   instance_type = "t2.micro"
-  subnet_id = "${module.public_subnet.subnet_id[count.index]}"
   associate_public_ip_address = true
-  vpc_security_group_ids = ["${module.http_security_group.id}"]
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
               nohup busybox httpd -f -p 8080 &
               EOF
-  tags {
-    Name = "busybox-web"
+  security_groups = ["${module.http_security_group.id}"]
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "autoscaling_group" {
+  name = "web-asg"
+  launch_configuration = "${aws_launch_configuration.web_asg.name}"
+  max_size = "5"
+  min_size = "1"
+  desired_capacity = "2"
+  vpc_zone_identifier = ["${module.public_subnet.subnet_id}"]
+  health_check_grace_period = "60"
+  wait_for_capacity_timeout = "10m"
+  health_check_type = "EC2"
+  force_delete = false
+  enabled_metrics = [
+    "GroupMinSize",
+    "GroupMaxSize",
+    "GroupDesiredCapacity",
+    "GroupInServiceInstances",
+    "GroupPendingInstances",
+    "GroupStandbyInstances",
+    "GroupTerminatingInstances",
+    "GroupTotalInstances",
+  ]
+  lifecycle {
+    create_before_destroy = true
   }
 }
